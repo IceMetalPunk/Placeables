8def82e6bd801490abb7e3c5fcf46100f6041068
IceMetalPunk <icemetalpunk@gmail.com>
2017-11-26 00:16:24 -0500

    Set metadata.

---
+++ src/main/java/com/icemetalpunk/placeables/Placeables.java
@@ -0,0 +1,15 @@
+package com.icemetalpunk.placeables;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Placeables.MODID, version = Placeables.VERSION)
+public class Placeables {
+	public static final String MODID = "placeables";
+	public static final String VERSION = "1.0";
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+	}
+}
