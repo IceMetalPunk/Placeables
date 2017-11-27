3bca2c60d0cfa7ad45a717011fe19a31ced74d06
IceMetalPunk <icemetalpunk@gmail.com>
2017-11-27 04:40:52 -0500

    Added TODO list

--- src/main/java/com/icemetalpunk/placeables/Placeables.java
+++ src/main/java/com/icemetalpunk/placeables/Placeables.java
@@ -1,3 +1,15 @@
+/* TODO: Next
+ * 1. Placeable slime balls: slow-down on any block with slime layer
+ * 2. Placeable sugar: attract tameable animals
+ * 3. Placeable leaf: decraft 1 leaves block into 9 leafs, placeable in 8 rotations on floor as decoration
+ * 4. Placeable stone: decraft 1 cobblestone into 9 stones, placeable on floor as decoration, throwable for mild damage
+ * 5. Dyes directly placeable onto blocks with color properties (handle bone meal appropriately!)
+ * 6. Placeable ghast tears (trails)?: Maybe makes things slippery and "guides/pushes" entities along, like a conveyor?
+ * 7. Placeable blaze powder: Acts like magma blocks with "hot floor" damage? How can we make this more unique?
+ * 8. Placeable magma cream?: Layers like slime balls, instant fire extinguishing. Placeable in Nether!
+ * 9. Placeable Nether star?: Floats above ground like item without bob; cures negative potions in radius?
+ */
+
 package com.icemetalpunk.placeables;
 
 import com.icemetalpunk.placeables.events.MainEventHandler;
